0a984a2a1d7f464894cb04494b817bbf97e5ad4c
Guidosalimbeni <guido.salimbeni@gmail.com>
2020-12-24 18:00:48 +0000

    semantic UI https://react.semantic-ui.com/elements/icon/#variations-size

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import 'semantic-ui-css/semantic.min.css';
 
 const Root = () => (
     <Router>
