ee5402ff44e6ac04098fd5a3a420dcd455ab19c9
Guidosalimbeni <guido.salimbeni@gmail.com>
2020-12-26 15:34:50 +0000

    spinner

--- src/index.js
+++ src/index.js
@@ -10,32 +10,56 @@ import Register from './components/auth/Register';
 import firebase from "./firebase";
 import 'semantic-ui-css/semantic.min.css';
 
+import { createStore } from "redux";
+import { Provider, connect } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+import Spinner from "./Spinner"
+
+import rootReducer from "./reducers";
+import { setUser } from "./actions";
+
+const store = createStore(rootReducer, composeWithDevTools());
+
 class Root extends React.Component {
     componentDidMount() {
-      firebase.auth().onAuthStateChanged(user => {
-        if (user) {
-          this.props.history.push("/");
-        }
-      });
+        firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                this.props.setUser(user);
+                this.props.history.push("/");
+            }
+        });
     }
-  
-    render() {
-      return (
-        <Switch>
-          <Route exact path="/" component={App} />
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
-        </Switch>
-      );
-    }
-  }
 
-const RootwithAuth = withRouter(Root);
+    render() {
+        return this.props.isLoading ? <Spinner /> : (
+            <Switch>
+                <Route exact path="/" component={App} />
+                <Route path="/login" component={Login} />
+                <Route path="/register" component={Register} />
+            </Switch>
+        );
+    }
+}
+
+//const RootwithAuth = withRouter(Root);
+
+const mapStateFromProps = state => ({
+
+    isLoading: state.user.isLoading
+});
+
+
+const RootWithAuth = withRouter(
+    connect(mapStateFromProps, 
+        { setUser })(Root));
 
 ReactDOM.render(
-    <Router>
-        <RootwithAuth />
-    </Router>,
+    <Provider store={store}>
+        <Router>
+            <RootWithAuth />
+        </Router>
+    </Provider>,
     document.getElementById('root')
 );
 registerServiceWorker();
