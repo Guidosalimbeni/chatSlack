90b93cd8bc3b6cecd2e55394c2eba743e6428079
Guidosalimbeni <guido.salimbeni@gmail.com>
2020-12-26 17:21:43 +0000

    login and state for logout

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import Spinner from "./Spinner"
 
 import rootReducer from "./reducers";
-import { setUser } from "./actions";
+import { setUser, clearUser } from "./actions";
 
 const store = createStore(rootReducer, composeWithDevTools());
 
@@ -27,6 +27,10 @@ class Root extends React.Component {
             if (user) {
                 this.props.setUser(user);
                 this.props.history.push("/");
+            }
+            else{
+                this.props.history.push("/login");
+                this.props.clearUser();
             }
         });
     }
@@ -52,7 +56,7 @@ const mapStateFromProps = state => ({
 
 const RootWithAuth = withRouter(
     connect(mapStateFromProps, 
-        { setUser })(Root));
+        { setUser, clearUser })(Root));
 
 ReactDOM.render(
     <Provider store={store}>
