fcd2ba74d4b453f9d57c6d0eb806de7e48e5097d
Guidosalimbeni <guido.salimbeni@gmail.com>
2020-12-27 19:21:04 +0000

    set redux channel state

--- src/actions/index.js
+++ src/actions/index.js
@@ -17,3 +17,15 @@ export const clearUser = () => {
         
     }
 }
+
+export const setCurrentChannel = channel => {
+    return {
+        type: actionTypes.SET_CURRENT_CHANNEL,
+        payload: {
+            currentChannel : channel
+        }
+    }
+    
+}
+        
+ 
